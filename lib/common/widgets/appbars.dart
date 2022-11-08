f0d0e8d02f3cddd06149bf69d75c463cec03d8d7
Debola <adebolasanni079@gmail.com>
2022-11-08 06:24:12 +0100

    modified appbar to accommodate cart screen appbar

--- lib/common/widgets/appbars.dart
+++ lib/common/widgets/appbars.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:jumier/constants.dart';
+import 'package:jumier/features/cart/cart_screen.dart';
+import 'package:jumier/features/feed/screens/feed_screen.dart';
+import 'package:jumier/features/help/screens/help_screen.dart';
+import 'package:jumier/features/home/screens/category_screen.dart';
+import 'package:jumier/features/home/screens/home_screen.dart';
 import 'package:jumier/features/search/screens/search_screen.dart';
+import 'package:jumier/features/user/screens/account_screen.dart';
 
 PreferredSizeWidget getAppbar({
   required String title,
   required BuildContext context,
   Widget? titleWidget,
+  bool? isCart = false,
 }) {
   return PreferredSize(
     preferredSize: const Size.fromHeight(60),
@@ -25,42 +32,60 @@ PreferredSizeWidget getAppbar({
                   ),
                 ),
           ),
-          if (titleWidget == null)
+          if (!isCart!)
+            if (titleWidget == null)
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 4),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return SearchScreen();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Icon(
+                    Icons.search_outlined,
+                    size: 22,
+                  ),
+                ),
+              ),
+          if (!isCart)
             Padding(
               padding: const EdgeInsets.only(left: 15, right: 4),
               child: GestureDetector(
                 onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) {
-                        return SearchScreen();
-                      },
-                    ),
-                  );
+                  Navigator.pushNamed(context, CartScreen.routeName);
                 },
                 child: const Icon(
-                  Icons.search_outlined,
+                  Icons.shopping_cart_outlined,
                   size: 22,
                 ),
               ),
             ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 4),
-            child: GestureDetector(
-              onTap: () {},
-              child: const Icon(
-                Icons.shopping_cart_outlined,
-                size: 22,
-              ),
-            ),
-          ),
         ],
       ),
       titleTextStyle: const TextStyle(
         color: Colors.white,
         fontSize: 16,
       ),
+      actions: [
+        if (isCart)
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 4),
+            child: PopupMenuButton(
+              itemBuilder: (context) {
+                return cartChoices.keys
+                    .toList()
+                    .map((e) => PopupMenuItem(child: Text(e)))
+                    .toList();
+              },
+            ),
+          ),
+      ],
     ),
   );
 }
@@ -104,3 +129,11 @@ PreferredSizeWidget getSearchAppbar({
     ),
   );
 }
+
+const cartChoices = {
+  'home': HomeScreen(),
+  'categories': CategoryScreen(),
+  'feed': FeedScreen(),
+  'account': AccountScreen(),
+  'help': HelpScreen(),
+};
