64edfd4efde6c647d10c079b8537b5bcad7844db
debola-is <adebolasanni079@gmail.com>
2022-11-11 17:43:15 +0100

    refactored the class to allow for optional padding input parameter

--- lib/features/user/widgets/info_tab.dart
+++ lib/features/user/widgets/info_tab.dart
@@ -3,23 +3,25 @@ import 'package:jumier/constants.dart';
 
 class InfoTab extends StatelessWidget {
   final String description;
+  final EdgeInsets? padding;
 
   const InfoTab({
     super.key,
     required this.description,
+    this.padding = const EdgeInsets.only(
+      left: 15,
+      top: 20,
+      bottom: 10,
+    ),
   });
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.bottomLeft,
-      padding: const EdgeInsets.only(
-        left: 15,
-        top: 20,
-        bottom: 10,
-      ),
+      padding: padding,
       color: backgroundGrey,
       child: Text(
-        description,
+        description.toUpperCase(),
         style: const TextStyle(
           color: Colors.black45,
           fontSize: 12,
