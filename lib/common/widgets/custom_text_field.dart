4d7ad3251adfdaf8e9366c4faff34542ce1d8120
Debola <adebolasanni079@gmail.com>
2023-01-29 19:16:43 +0100

    made custom text field accept an optional parameter for the initial value

--- lib/common/widgets/custom_text_field.dart
+++ lib/common/widgets/custom_text_field.dart
@@ -3,17 +3,21 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
+  final String? initialText;
   final int maxLines;
   final TextInputType? inputType;
   final bool obscureText;
+  final bool? readOnly;
 
   const CustomTextField({
     Key? key,
     required this.controller,
     required this.hintText,
+    this.initialText,
     this.maxLines = 1,
     this.obscureText = false,
     this.inputType,
+    this.readOnly,
   }) : super(key: key);
 
   @override
@@ -23,7 +27,9 @@ class CustomTextField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       keyboardType: inputType,
+      initialValue: initialText,
       obscureText: obscureText,
+      readOnly: readOnly == null ? false : readOnly!,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(
