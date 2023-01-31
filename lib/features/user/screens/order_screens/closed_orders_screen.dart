4d007ea98deba9f717a21f19c5e699d962d47360
Debola <adebolasanni079@gmail.com>
2023-01-31 23:57:22 +0100

    created closed oreders tab bar view

---
+++ lib/features/user/screens/order_screens/closed_orders_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClosedOrdersScreen extends StatelessWidget {
+  const ClosedOrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'This is the closed orders page',
+      ),
+    );
+  }
+}
