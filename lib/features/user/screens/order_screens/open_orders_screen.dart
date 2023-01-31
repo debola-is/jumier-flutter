75d54f3c8c6abc35c1f328c25e0e32326ed6015d
Debola <adebolasanni079@gmail.com>
2023-01-31 23:57:43 +0100

    created open oreders tab bar view

---
+++ lib/features/user/screens/order_screens/open_orders_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class OpenOrdersScreen extends StatelessWidget {
+  const OpenOrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'This is the open orders page',
+      ),
+    );
+  }
+}
