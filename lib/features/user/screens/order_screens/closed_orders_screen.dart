dec4dac838d40c9c4e16d56b35e4a72fefcd8faf
Debola <adebolasanni079@gmail.com>
2023-02-12 08:58:21 +0100

    populated the open and closed orders tabviews bodies with list view of single order widgets

--- lib/features/user/screens/order_screens/closed_orders_screen.dart
+++ lib/features/user/screens/order_screens/closed_orders_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:jumier/constants.dart';
+import 'package:jumier/features/user/widgets/single_order_widget.dart';
 
 class ClosedOrdersScreen extends StatelessWidget {
   const ClosedOrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'This is the closed orders page',
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      color: backgroundGrey,
+      child: ListView.separated(
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return const SingleOrderWidget();
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            height: 10,
+          );
+        },
+        itemCount: 10,
       ),
     );
   }
--- lib/features/user/screens/order_screens/open_orders_screen.dart
+++ lib/features/user/screens/order_screens/open_orders_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:jumier/constants.dart';
+import 'package:jumier/features/user/widgets/single_order_widget.dart';
 
 class OpenOrdersScreen extends StatelessWidget {
   const OpenOrdersScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'This is the open orders page',
+    return Container(
+      margin: const EdgeInsets.only(top: 10),
+      color: backgroundGrey,
+      child: ListView.separated(
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context, index) {
+          return const SingleOrderWidget();
+        },
+        separatorBuilder: (context, index) {
+          return const SizedBox(
+            height: 10,
+          );
+        },
+        itemCount: 10,
       ),
     );
   }
