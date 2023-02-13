7c8235519e9eeeaf64b10424a0b313a88f90b7c4
Debola <adebolasanni079@gmail.com>
2023-02-13 20:26:15 +0100

    order model created

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,41 +1,57 @@
 import 'dart:convert';
 import 'package:jumier/models/product.dart';
-import 'package:jumier/models/user.dart';
 
 class Order {
-  final User user;
   final List<Product> products;
-  final double? totalPrice;
-  final String dateCreated;
-  final int? status;
-  final String? id;
+  final List<int> quantity;
+  final double totalPrice;
+  final int timeOfOrder;
+  final int status;
+  final String id;
+  final String userId;
+  final String deliveryAddress;
+  // Address string is a combination of  various components: house number, street name, city name and state name. All these components should be joined at the server level by a double percentage sign "%%"
 
   Order({
-    required this.user,
     required this.products,
-    this.totalPrice,
-    this.id,
-    required this.dateCreated,
-    this.status,
+    required this.quantity,
+    required this.totalPrice,
+    required this.timeOfOrder,
+    required this.status,
+    required this.id,
+    required this.userId,
+    required this.deliveryAddress,
   });
 
   factory Order.fromMap(Map<String, dynamic> map) {
     return Order(
-      user: map['user'] ?? '',
-      products: map['products'] ?? [],
-      dateCreated: map['dateCreated'] ?? '',
-      id: map['_id'] ?? '',
+      products: List<Product>.from(
+        map['products']?.map(
+          (x) => Product.fromMap(
+            x['product'],
+          ),
+        ),
+      ),
+      quantity: List<int>.from(
+        map['products']?.map(
+          (x) => x['quantity'],
+        ),
+      ),
       totalPrice: map['totalPrice']?.toDouble() ?? 0.0,
+      timeOfOrder: map['timeOfOrder']?.toInt() ?? 0,
       status: map['status']?.toInt() ?? 0,
+      id: map['_id'] ?? '',
+      userId: map['userId'] ?? '',
+      deliveryAddress: map['deliveryAddress'] ?? '',
     );
   }
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> data = {};
-    data['user'] = user;
-    data['products'] = products;
-    data['dateCreated'] = dateCreated;
+    data['products'] = products.map((e) => e.toMap()).toList();
+    data['quantity'] = quantity;
     data['totalPrice'] = totalPrice;
+    data['timeOfOrder'] = timeOfOrder;
     data['status'] = status;
     data['_id'] = id;
     return data;
