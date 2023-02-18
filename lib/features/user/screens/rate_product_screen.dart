38c489682bfc388b7ecc5760e36138b24ac29cec
Debola <adebolasanni079@gmail.com>
2023-02-18 22:08:41 +0100

    added a new function to update the interactive questions in review screen

--- lib/features/user/screens/rate_product_screen.dart
+++ lib/features/user/screens/rate_product_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:jumier/common/widgets/appbars.dart';
 import 'package:jumier/common/widgets/custom_button.dart';
 import 'package:jumier/common/widgets/custom_network_image.dart';
+import 'package:jumier/common/widgets/custom_text_field.dart';
 import 'package:jumier/constants.dart';
 
 class RateProductScreen extends StatefulWidget {
@@ -15,6 +16,44 @@ class RateProductScreen extends StatefulWidget {
 
 class _RateProductScreenState extends State<RateProductScreen> {
   double productRating = 0.0;
+
+// These are the interactive questions that reviewer will be asked based on their inputted rating.
+  String text1 = '';
+  String text2 = '';
+
+  //This function will change the value of the text
+
+  void updateQuestions(double rating) {
+    if (rating > 3.0) {
+      text1 = 'What did you like about this product?';
+    } else {
+      text1 = 'What did you dislike about this product?';
+    }
+
+    if (rating > 4.0) {
+      text2 = 'What did you like the most about it?';
+    } else if (rating > 3.0) {
+      text2 = 'How can we make it perfect?';
+    } else if (rating > 2.0) {
+      text2 = 'Could it be better?';
+    } else {
+      text2 = 'How can we improve it?';
+    }
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _reviewController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _titleController.dispose();
+    _reviewController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,6 +129,7 @@ class _RateProductScreenState extends State<RateProductScreen> {
               RatingBar.builder(
                 itemPadding: const EdgeInsets.all(5.0),
                 itemSize: 35,
+                minRating: 1.0,
                 unratedColor: Colors.grey.shade300,
                 itemBuilder: (context, index) => Icon(
                   Icons.star_rounded,
@@ -97,6 +137,8 @@ class _RateProductScreenState extends State<RateProductScreen> {
                 ),
                 onRatingUpdate: (value) => setState(() {
                   productRating = value;
+                  _nameController.text = 'User\'s name';
+                  updateQuestions(value);
                 }),
               ),
               if (productRating > 0.0)
@@ -108,23 +150,36 @@ class _RateProductScreenState extends State<RateProductScreen> {
                       Container(
                         alignment: Alignment.center,
                         padding: const EdgeInsets.only(top: 10, bottom: 35),
-                        child: const Text(
-                          'What did you like about this product?',
-                          style: TextStyle(fontWeight: FontWeight.w500),
+                        child: Text(
+                          text1,
+                          style: const TextStyle(fontWeight: FontWeight.w500),
                         ),
                       ),
                       const SizedBox(
                         height: 40,
                       ),
                       Text(
-                        'How can we make it perfect?'.toUpperCase(),
+                        text2.toUpperCase(),
                         style: const TextStyle(
                             color: Colors.grey,
                             fontSize: 12,
                             fontWeight: FontWeight.w500),
                       ),
-                      const TextField(
-                        decoration: InputDecoration(hintText: 'Review Title'),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      TextField(
+                        controller: _titleController,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: 'Review Title',
+                          hintStyle: TextStyle(
+                            color: Colors.grey.shade400,
+                            fontSize: 14,
+                          ),
+                        ),
                       ),
                       const SizedBox(height: 20.0),
                       Text(
@@ -132,9 +187,29 @@ class _RateProductScreenState extends State<RateProductScreen> {
                         style: TextStyle(
                             color: Colors.grey.shade400, fontSize: 12.0),
                       ),
-                      const TextField(
+                      const SizedBox(
+                        height: 5.0,
+                      ),
+                      TextField(
+                        controller: _reviewController,
+                        style: const TextStyle(
+                          fontSize: 14,
+                        ),
+                        maxLines: 2,
                         decoration: InputDecoration(
-                            hintText: 'How can we make it perfect?'),
+                          hintText: text2,
+                          hintStyle: TextStyle(
+                              color: Colors.grey.shade400, fontSize: 14),
+                          contentPadding: const EdgeInsets.all(15.0),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Colors.grey.shade400, width: 2.5),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: shadeOfOrange, width: 2.5),
+                          ),
+                        ),
                       ),
                       const SizedBox(height: 20.0),
                       Text(
@@ -142,8 +217,17 @@ class _RateProductScreenState extends State<RateProductScreen> {
                         style: TextStyle(
                             color: Colors.grey.shade400, fontSize: 12.0),
                       ),
-                      const TextField(
-                        decoration: InputDecoration(hintText: 'User Name'),
+                      TextField(
+                        controller: _nameController,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: 'Enter your name',
+                          hintStyle: TextStyle(
+                              color: Colors.grey.shade400, fontSize: 14),
+                        ),
                       ),
                       const SizedBox(height: 60),
                       CustomButton(text: 'submit review', onTap: () {})
