38fd1bbe9ac4bd6443e4e7c0599c8da0e61572d8
Debola <adebolasanni079@gmail.com>
2023-10-05 02:28:54 +0100

    added custom modal barrier to display loading animation and prevent user input while performing network request

--- lib/common/widgets/custom_loaders.dart
+++ lib/common/widgets/custom_loaders.dart
@@ -1,65 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:jumier/global_variables.dart';
 
-class NetworkLoadingAnimation extends StatefulWidget {
-  const NetworkLoadingAnimation({Key? key}) : super(key: key);
-
-  @override
-  State<NetworkLoadingAnimation> createState() =>
-      _NetworkLoadingAnimationState();
-}
-
-class _NetworkLoadingAnimationState extends State<NetworkLoadingAnimation>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController = AnimationController(
-    vsync: this,
-    duration: const Duration(
-      seconds: 2,
-    ),
-  )..repeat();
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    _animationController.dispose();
-  }
+class CustomNetworkLoadingBarrier extends StatelessWidget {
+  final Widget child;
+  final bool isloading;
+  const CustomNetworkLoadingBarrier({
+    super.key,
+    required this.child,
+    required this.isloading,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) {
-          return Transform.rotate(
-            angle: _animationController.value * 2 * 3.14,
-            child: SizedBox(
-                height: 50,
-                width: 50,
-                child: Image.asset('assets/images/star.png')),
-          );
-        });
-  }
-}
-
-class ImageLoadingAnimation extends StatelessWidget {
-  final double? height;
-  final double? width;
-
-  const ImageLoadingAnimation({
-    Key? key,
-    this.height = 100,
-    this.width,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: height,
-      width: width ?? height,
-      child: const Center(
-        child: CircularProgressIndicator(
-          strokeWidth: 2,
-        ),
-      ),
+    return Stack(
+      children: [
+        child,
+        if (isloading)
+          const Opacity(
+            opacity: 0.7,
+            child: ModalBarrier(
+              dismissible: false,
+              color: Colors.black,
+            ),
+          ),
+        if (isloading)
+          Center(
+            child: CircularProgressIndicator(
+              color: shadeOfOrange,
+            ),
+          ),
+      ],
     );
   }
 }
