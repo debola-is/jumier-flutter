66d0ffb233610e484678239b3843a0d95075ef74
Debola <adebolasanni079@gmail.com>
2023-10-05 02:07:47 +0100

    added constants file for edgeinsets and paddings

---
+++ lib/constants.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/widgets.dart';
+
+const SizedBox yBox4 = SizedBox(height: 4);
+const SizedBox yBox8 = SizedBox(height: 8);
+const SizedBox yBox12 = SizedBox(height:  12);
+const SizedBox yBox16 = SizedBox(height:  16);
+const SizedBox yBox24 = SizedBox(height:  24);
+const SizedBox yBox32 = SizedBox(height:  32);
+const SizedBox yBox48 = SizedBox(height:  48);
+
+const SizedBox xBox4 = SizedBox(width: 4);
+const SizedBox xBox8 = SizedBox(width: 8);
+const SizedBox xBox12 = SizedBox(width:  12);
+const SizedBox xBox16 = SizedBox(width:  16);
+const SizedBox xBox24 = SizedBox(width:  24);
+const SizedBox xBox32 = SizedBox(width:  32);
+const SizedBox xBox48 = SizedBox(width:  48);
+const SizedBox xBox64 = SizedBox(width:  64);
+const SizedBox xBox72 = SizedBox(width:  72);
+const SizedBox xBox100 = SizedBox(width:  100);
+const SizedBox xBox120 = SizedBox(width:  120);
+const SizedBox xBox150 = SizedBox(width:  150);
+const SizedBox xBox200 = SizedBox(width:  200);
+const SizedBox xBox250 = SizedBox(width:  250);
+
+const EdgeInsets p4 = EdgeInsets.all(4);
+const EdgeInsets p8 = EdgeInsets.all(8);
+const EdgeInsets p12 = EdgeInsets.all(12);
+const EdgeInsets p16 = EdgeInsets.all(16);
+const EdgeInsets p24 = EdgeInsets.all(24);
+const EdgeInsets p32 = EdgeInsets.all(32);
+const EdgeInsets p48 = EdgeInsets.all(48);
+
+const EdgeInsets px4 = EdgeInsets.symmetric(horizontal: 4);
+const EdgeInsets px8 = EdgeInsets.symmetric(horizontal: 8);
+const EdgeInsets px12 = EdgeInsets.symmetric(horizontal: 12);
+const EdgeInsets px16 = EdgeInsets.symmetric(horizontal: 16);
+const EdgeInsets px24 = EdgeInsets.symmetric(horizontal: 24);
+const EdgeInsets px32 = EdgeInsets.symmetric(horizontal: 32);
+const EdgeInsets px48 = EdgeInsets.symmetric(horizontal: 8);
+
+const EdgeInsets py4 = EdgeInsets.symmetric(vertical: 4);
+const EdgeInsets py8 = EdgeInsets.symmetric(vertical: 8);
+const EdgeInsets py12 = EdgeInsets.symmetric(vertical: 12);
+const EdgeInsets py16 = EdgeInsets.symmetric(vertical: 16);
+const EdgeInsets py24 = EdgeInsets.symmetric(vertical: 24);
+const EdgeInsets py32 = EdgeInsets.symmetric(vertical: 32);
+const EdgeInsets py48 = EdgeInsets.symmetric(vertical: 8);
