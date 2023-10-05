38718646405bc5561bda790cdb5fc1ba5c71bf50
Debola <adebolasanni079@gmail.com>
2023-10-05 11:55:07 +0100

    ProductVariant model updated with filestore id for images store

--- lib/models/product_variant.dart
+++ lib/models/product_variant.dart
@@ -2,24 +2,36 @@ import 'dart:convert';
 
 class ProductVariant {
   final String name;
+  final String image;
   final double price;
+  final int quantity;
+  final String fileStoreId;
 
   ProductVariant({
     required this.name,
     required this.price,
+    required this.image,
+    required this.quantity,
+    required this.fileStoreId,
   });
 
   Map<String, dynamic> toMap() {
     return {
-      'name': name,
-      'price': price,
+      "name": name,
+      "price": price,
+      "image": image,
+      "quantity": quantity,
+      "fileStoreId": fileStoreId,
     };
   }
 
   factory ProductVariant.fromMap(Map<String, dynamic> map) {
     return ProductVariant(
-      name: map['name'] ?? '',
-      price: map['price']?.toDouble() ?? 0.0,
+      name: map["name"] ?? '',
+      price: map["price"]?.toDouble() ?? 0.0,
+      quantity: map["quantity"]?.toInt() ?? 0,
+      image: map["image"] ?? [],
+      fileStoreId: map["fileStoreId"] ?? "",
     );
   }
 
