becf159d86e357110608758834e714b34bd098e1
Debola <adebolasanni079@gmail.com>
2023-10-05 02:19:12 +0100

    improve on validation process in signup function

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,12 +1,18 @@
+import 'dart:convert';
+import 'dart:developer' show log;
+import 'package:jumier/providers/user_provider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:jumier/common/utils/utils.dart';
-import 'package:jumier/global_variables.dart';
 import 'package:jumier/models/user.dart';
 import 'package:http/http.dart' as http;
-import 'dart:developer' show log;
+import 'package:provider/provider.dart';
+
+import '../config.dart';
 
 class AuthService {
   // TODO: Improve password validation using regex
+  // TODO: At main application initialization, perform sharedpreferences token validation. If token was generated more than a week ago, sign user out (in the background) and prompt them to sign in again via a dismissable dialogue box because token generated expires after 7days in the server side. This functinality can be taken further in other ways.
   //Validate password by checking length of characters
   bool validPassword({
     required String password,
@@ -17,7 +23,7 @@ class AuthService {
     return false;
   }
 
-  Future<bool> signUp({
+  Future<void> signUp({
     required BuildContext context,
     required String firstName,
     required String lastName,
@@ -31,9 +37,11 @@ class AuthService {
       lastName: lastName,
       phoneNumber: '',
       email: email,
+      password: password,
       addresses: [],
       token: '',
       id: '',
+      userType: '',
       cart: [],
       recentlyViewed: [],
       savedItems: [],
@@ -57,19 +65,64 @@ class AuthService {
             onSuccess: () {
               showSnackBar(
                 context,
-                'Account created successfully, login with the same credentials',
+                'Successful!',
                 ErrorType.success,
               );
             });
       } catch (e) {
         log(e.toString());
+
         showSnackBar(
           context,
           'Error occured, could not sign user up',
           ErrorType.error,
         );
       }
+    } else {
+      showSnackBar(context, "Password is too weak", ErrorType.error);
     }
-    return true;
+  }
+
+  Future<void> signIn({
+    required BuildContext context,
+    required String email,
+    required String password,
+    String? middleName,
+  }) async {
+    final params = {"email": email, "password": password};
+
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$serverUrl/api/v0/auth/signin'),
+        body: jsonEncode(params),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      httpErrorHandler(
+          response: response,
+          context: context,
+          onSuccess: () {
+            showSnackBar(
+              context,
+              'Sign-in success',
+              ErrorType.success,
+            );
+            Navigator.pop(context);
+            Provider.of<UserProvider>(context, listen: false)
+                .setUser(response.body);
+            log(Provider.of<UserProvider>(context, listen: false)
+                .user
+                .userType);
+          });
+    } catch (e) {
+      log(e.toString());
+      showSnackBar(
+        context,
+        'Sign-in failed, try another time',
+        ErrorType.error,
+      );
+    }
   }
 }
