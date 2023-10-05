828d6f101f24d84c40a56e78dac38739183e779b
Debola <adebolasanni079@gmail.com>
2023-10-05 12:04:17 +0100

    Action button now contains restrictedToSignedInUser field which is user to deternmine signin access to the action

--- lib/features/user/widgets/account_action.dart
+++ lib/features/user/widgets/account_action.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jumier/common/utils/restricted_access_page.dart';
+import 'package:jumier/providers/user_provider.dart';
+import 'package:provider/provider.dart';
 
 class AccountAction extends StatelessWidget {
   final VoidCallback onTap;
@@ -11,6 +15,7 @@ class AccountAction extends StatelessWidget {
   final Widget? titleWidget;
   final Widget? trailingWidget;
   final TextStyle? titleTextStyle;
+  final bool restictedToSignedInUser;
   const AccountAction({
     Key? key,
     required this.onTap,
@@ -23,14 +28,25 @@ class AccountAction extends StatelessWidget {
     this.titleTextStyle,
     this.leadingWidget,
     this.titleWidget,
+    this.restictedToSignedInUser = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = context.watch<UserProvider>().user;
     return ListTile(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       tileColor: Colors.white,
-      onTap: onTap,
+      onTap: restictedToSignedInUser && user.token.isEmpty
+          ? () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => RestrictedAccessPage(title: title),
+                ),
+              );
+            }
+          : onTap,
       enabled: enabled!,
       contentPadding: EdgeInsets.symmetric(
         horizontal: leadingWidget == null ? 15 : 0,
